0a10d8310e3d756dd6159aebef2b9ed3f846500e
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-08-29 17:34:45 -0300

    string

--- type-string.js
+++ type-string.js
@@ -6,4 +6,16 @@
   const stringdeNumeros = "12345";
 
   // posso usar as duas " " quando for citar algo
-  const citação = 'o leo disse "oi"'
+ // const citacao = 'o leo disse "oi"'
+
+ // console.log(citacao)
+
+  //usando template string ou literal // 
+
+  // usando concatenação  (+)
+   const frase = "ola, sou a "
+
+   const nome = "julhia"
+
+
+   console.log(frase + nome)
