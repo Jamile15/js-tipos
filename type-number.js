97a7447447c19ea93d0a6676eba468de03046b96
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-08-29 17:23:45 -0300

    first commit

---
+++ type-number.js
@@ -0,0 +1,29 @@
+// tipo nunber
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+
+const segundoNumero = 3;
+
+const operacaomatematica = primeiroNumero  * segundoNumero;
+
+console.log(operacaomatematica)
+
+
+//ponto flutuante
+
+const pontoFlutuante = 3.3;
+const pontoFlutuantesemZero= .5
+
+const operacao= pontoFlutuante / pontoFlutuantesemZero;
+
+console.log(operacao)
+
+//not a number = não e um numero
+
+const alura = 'alura';
+
+const gratis = 'gratis'
+
+console.log(alura + gratis)
---
+++ type-string.js
@@ -0,0 +1,9 @@
+//type string
+  const texto1= "óla mundo!";
+  const texto2 = 'ola mundo!';
+
+  const senha = "supersegura123456!";
+  const stringdeNumeros = "12345";
+
+  // posso usar as duas " " quando for citar algo
+  const citação = 'o leo disse "oi"'
