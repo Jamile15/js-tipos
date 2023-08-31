3dda5a0a8246b38430dd7c10412fc30d72faf555
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-08-31 11:36:21 -0300

    usando boolean

---
+++ type-boolean.js
@@ -0,0 +1,33 @@
+//type boolean = false e true
+
+//verificar se uma coisa e verdadeira ou falça faz comparações
+// true -> verdadeiro
+//false -> Falço 
+
+// um = atribui um valor a uma variavel
+// == ou === são de comparações
+
+/*----------------------------COMPARANDO NUMEROS---------------------------------------------------------
+const primeiroNumero = 10;
+
+const segundoNumero = 1
+
+let cadastroAtivado
+
+if(primeiroNumero === segundoNumero){
+    console.log("aee deu certoo o valor é igual")
+} else{console.log("não não, ta diferente isso ae")}
+
+--------------------------------- ----------------------------------------------------------------------*/
+
+/*-----------------------------COMPARANDO STRING-------------------------------------------------------------
+const texto1 = "a";
+const texto2 = "a";
+
+if(texto1 === texto2){
+    console.log("sua senha esta correta")
+}else{
+    console.log("sua senha esta ERRADA tenta de novoooooooooooo")
+
+}
+--------------------------------------------------------------------------------------------------------*/
