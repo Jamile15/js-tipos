e05ad8348d280875f5b1852e08aebd3dc87ba85d
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-09-06 10:55:29 -0300

    funçoes

---
+++ funcoes.js
@@ -0,0 +1,16 @@
+//let  x = "";
+//console.log(x);
+ //x = "OI";
+
+ //1° declara 
+                        //string
+ function imprimeTexto (texto){
+    console.log(texto);
+ }
+
+
+ //2° executa a função uma ou mais vezes
+
+imprimeTexto("oi")
+imprimeTexto("jamile")
+
