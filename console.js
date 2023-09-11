be66ccd3680205efee73432f53a7227dce1e24b8
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-09-11 09:16:39 -0300

    conlole

---
+++ console.js
@@ -0,0 +1,14 @@
+//log -> registro
+const minhaVar = true
+//console.log (245)
+//console.log ("eu sou um texto")
+//console.log (minhaVar)
+
+//tratamento de erro!
+const numero = "225";
+
+if(numero === 225){
+    console.log ("parabens")
+} else{
+
+console.error("deu erro!")}
