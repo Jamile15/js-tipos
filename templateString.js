f1e4c48a2891cf0c742bdf67436a251c5298053a
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-09-13 15:56:12 -0300

    template string

---
+++ templateString.js
@@ -0,0 +1,10 @@
+//
+
+const nome = "juh"
+const idade = 15;
+const cidnasc = "sao paulo"
+
+
+const apresentacaotemplate = `meu nome é ${nome}, e minha idade e ${idade} nasci em ${cidnasc} `
+
+console.log(apresentacaotemplate)
